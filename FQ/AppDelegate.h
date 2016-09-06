9bbd0274b038237ce6b13fced65e1c97efaa28bc
wangaiguo <wangaiguo@wangaiguodeMac-mini.local>
2016-09-06 11:00:32 +0800

    Initial Commit

---
+++ FQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FQ
+//
+//  Created by wangaiguo on 16/9/6.
+//  Copyright © 2016年 wangaiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
